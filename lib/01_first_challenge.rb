beae76b27821a6d019edc2616caf5d4354af1e53
Mary Dick <maryclairedick@gmail.com>
2020-08-14 21:58:30 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -5,7 +5,7 @@ def first_challenge
        
        :matriarch => {name: "Lady Montague", age: "54"},
        
-       :hero => { name: "Romeo", age: "15", status: "alive"},
+       :hero => {name: "Romeo", age: "15", status: "alive"},
        
        :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
@@ -16,32 +16,16 @@ def first_challenge
        
      :capulet => {
        
-       :patriarch => {name: "Lord Capulet", age: "50"
-       },
+       :patriarch => {name: "Lord Capulet", age: "50"},
        
-       :matriarch => {
-         name: "Lady Capulet",
-         age: "51"
-       },
+       :matriarch => {name: "Lady Capulet", age: "51"},
        
-       :heroine => {
-         name: "Juliet",
-         age: "15",
-         status: "alive"
-       },
+       :heroine => {name: "Juliet", age: "15", status: "alive"},
        
        :heroine_friends => [
-         {
-           name: "Steven",
-           age: "30",
-           attitude: "confused"
-         },
+         {name: "Steven", age: "30", attitude: "confused"},
          
-         {
-           name: "Nurse",
-           age: "44",
-           attitude: "worried"
-         }
+         {name: "Nurse", age: "44", attitude: "worried"}
          ]
      }
     }
