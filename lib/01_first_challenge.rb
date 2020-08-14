139ef3e5665dee530df1fe8926f2f1f1c3961d5a
Mary Dick <maryclairedick@gmail.com>
2020-08-14 22:00:12 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -12,21 +12,22 @@ def first_challenge
          
       #   {name: "Mercutio", age: "18", attitude: "hot-headed"}
       #   ]
-      # },
+      # 
+      },
        
      :capulet => {
        
-       :patriarch => {name: "Lord Capulet", age: "50"},
+      # :patriarch => {name: "Lord Capulet", age: "50"},
        
-       :matriarch => {name: "Lady Capulet", age: "51"},
+      # :matriarch => {name: "Lady Capulet", age: "51"},
        
-       :heroine => {name: "Juliet", age: "15", status: "alive"},
+      # :heroine => {name: "Juliet", age: "15", status: "alive"},
        
-       :heroine_friends => [
-         {name: "Steven", age: "30", attitude: "confused"},
+      # :heroine_friends => [
+      #   {name: "Steven", age: "30", attitude: "confused"},
          
-         {name: "Nurse", age: "44", attitude: "worried"}
-         ]
+      #   {name: "Nurse", age: "44", attitude: "worried"}
+      #   ]
      }
     }
 
