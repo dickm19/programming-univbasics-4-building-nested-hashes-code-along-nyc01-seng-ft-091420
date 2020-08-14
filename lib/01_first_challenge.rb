0fc9841b55edebfa01d63b8cfc1abffd46e5a07f
Mary Dick <maryclairedick@gmail.com>
2020-08-14 21:57:32 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,44 +1,22 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
+       :patriarch => {name: "Lord Montague", age: "53"},
        
-       :patriarch => {
-         name: "Lord Montague",
-         age: "53"
-       },
+       :matriarch => {name: "Lady Montague", age: "54"},
        
-       :matriarch => {
-         name: "Lady Montague",
-         age: "54"
-       },
-       
-       :hero => {
-         name: "Romeo",
-         age: "15",
-         status: "alive"
-       },
+       :hero => { name: "Romeo", age: "15", status: "alive"},
        
        :hero_friends => [
+         {name: "Benvolio", age: "17", attitude: "worried"},
          
-         {
-           name: "Benvolio",
-           age: "17",
-           attitude: "worried"
-         },
-         
-         {
-           name: "Mercutio",
-           age: "18",
-           attitude: "hot-headed"
-         }
-       ]
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+         ]
+       },
        
-     },
      :capulet => {
        
-       :patriarch => {
-         name: "Lord Capulet",
-         age: "50"
+       :patriarch => {name: "Lord Capulet", age: "50"
        },
        
        :matriarch => {
